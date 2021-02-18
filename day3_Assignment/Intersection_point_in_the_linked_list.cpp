58a8ccdc180084fa85602da41ad36bdcf3a6a9e6
Chandan krishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2021-02-18 23:15:21 +0530

    Update Intersection_point_in_the_linked_list.cpp

--- day3_Assignment/Intersection_point_in_the_linked_list.cpp
+++ day3_Assignment/Intersection_point_in_the_linked_list.cpp
@@ -1,39 +1,71 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream> 
+using namespace std; 
+  
 
-class Node
+class Node 
+{ 
+    public:
+        int data; 
+        Node* link; 
+}; 
+  
+ 
+int getIntersectionNode(Node* head1, Node* head2) 
+{ 
+    Node *ptr1 = head1, *ptr2 = head2; 
+  
+    
+    while (ptr1 != ptr2) 
+    { 
+  
+        if (ptr1 == NULL) 
+            ptr1 = head2; 
+         
+        else  
+            ptr1 = ptr1->link; 
+         
+  
+        if (ptr2 == NULL) 
+            ptr2 = head1; 
+         
+        else  
+            ptr2 = ptr2->link; 
+        
+    } 
+  
+    
+    return ptr1->data; 
+} 
+  
+ 
+int main() 
 {
-public:
-    int data;
-    Node *nxt;
-    Node()
-    {
-        Node(0);
-    }
-
-    Node(int data)
-    {
-        this->data = data;
-        this->nxt = NULL;
-    }
-};
-
-void printList(Node *head)
-{
-    Node *ptr = head;
-    while (ptr != NULL)
-    {
-        cout << ptr->data << "->";
-        ptr = ptr->nxt;
-    }
-    cout << "\n";
-}
-
-int getSize(Node *head)
-{
-    Node *ptr = head;
-    int size = 0;
-    while (ptr != NULL)
+  
+    Node* newNode; 
+    Node* head1 = new Node(); 
+    head1->data = 10; 
+    Node* head2 = new Node(); 
+    head2->data = 3; 
+    newNode = new Node(); 
+    newNode->data = 6; 
+    head2->link = newNode; 
+    newNode = new Node(); 
+    newNode->data = 9; 
+    head2->link->link = newNode; 
+    newNode = new Node(); 
+    newNode->data = 15; 
+    head1->link = newNode; 
+    head2->link->link->link = newNode; 
+    newNode = new Node(); 
+    newNode->data = 30; 
+    head1->link->link = newNode; 
+    head1->link->link->link = NULL; 
+  
+     
+    cout << getIntersectionNode(head1, head2); 
+  
+    return 0; 
+} 
     {
         size++;
         ptr = ptr->nxt;
