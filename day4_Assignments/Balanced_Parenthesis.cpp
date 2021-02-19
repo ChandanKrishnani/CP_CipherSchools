16aa5bf66fe242cc7eaabb66114083f58e7a9ca9
Chandan krishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2021-02-19 23:37:56 +0530

    Create Balanced_Parenthesis.cpp

---
+++ day4_Assignments/Balanced_Parenthesis.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isMatching(char open, char close)
+{
+    return ((open == '(' && close == ')') || (open == '{' && close == '}') || (open == '[' && close == ']'));
+}
+
+bool isValid(string str)
+{
+    stack<char> S;
+    int n = str.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[')
+        {
+            S.push(str[i]);
+        }
+        else
+        {
+            if (S.empty())
+                return false;
+            
+            char open = S.top(); S.pop();
+            if (!isMatching(open, str[i]))
+                return false;
+        }
+    }
+    if (S.empty())
+        return true;
+    return false;
+}
+
+int main()
+{
+    string str={"()(){}[]"};
+
+    string str2={"(])(){}[]"};
+    isValid(str) ? cout << "\nYES" : cout << "\nFALSE";
+    isValid(str2) ? cout << "\nYES" : cout << "\nFALSE";
+    return 0;
+}
